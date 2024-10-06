e3661eee39f162c22bb3a27b9e316079f7227f2d
Jhonatan <jhonatan.sampaiof@gmail.com>
2024-10-06 17:32:18 -0300

    Add: Consulta, Evento entities

---
+++ KCIAO.API/Domain/Entities/ConsultaEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace KCIAO.API.Domain.Entities
+{
+    [Table("tb_consulta")]
+    public class ConsultaEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string id_consulta { get; set; }
+        public string profissional { get; set; }
+        public string local_consulta { get; set; }
+        public int horario_consulta { get; set; }
+    }
+}
---
+++ KCIAO.API/Domain/Entities/EventoEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KCIAO.API.Domain.Entities
+{
+    [Table("tb_evento")]
+    public class EventoEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string id_evento { get; set; }
+        public string tipo_evento { get; set; }
+        public string desc_evento { get; set; }
+        public DateOnly dt_evento { get; set; }
+    }
+}
